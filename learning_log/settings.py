3a5772aecfa63071738bc543514e01438d56ec5b
Matt Ashorn <matthew.ashorn@utexas.edu>
2022-04-12 16:01:27 -0500

    Moved private keys off of repo.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import user_env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h$b&86dpb-08nfovh83j2y@&ixm20fzq_1k9wm2_aa*ygow07w'
+SECRET_KEY = user_env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
