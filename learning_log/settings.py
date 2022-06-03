d3e343e775213c78beefbca031d66d59a57139aa
Matt Ashorn <matthew.ashorn@utexas.edu>
2022-06-03 06:48:07 -0500

    asdf

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from user_env import KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KEY
+SECRET_KEY = 'django-insecure-h$b&we349rtna6dpb-08tfovhy&n2apdmf2423f2q_5k9mmh_aa*ygow07w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
