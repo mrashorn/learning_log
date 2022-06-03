c6f928cf928ccee9a1683df6d92d423d7686ac89
Matt Ashorn <matthew.ashorn@utexas.edu>
2022-06-03 07:21:27 -0500

    Set DEBUG based on evironment variables

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -140,5 +140,5 @@ django_heroku.settings(locals())
 
 if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
-elif os.environ.get('DeBUG') == 'FALSE':
+elif os.environ.get('DEBUG') == 'FALSE':
     DEBUG = False
