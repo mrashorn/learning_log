7063f589620d1aefdee4934dfae020eb37bdd489
Matt Ashorn <matthew.ashorn@utexas.edu>
2022-06-03 07:16:23 -0500

    Set DEBUG based on evironment variables

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -134,5 +134,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'users:login'
 
 # Heroku Settings
+import os
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DeBUG') == 'FALSE':
+    DEBUG = False
